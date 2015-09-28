7683eb7c26de0f1e93174a124d1a22b5ba6c1aca
Marcos Mamede Ventura <marcos.m.ventura@gmail.com>
2015-09-27 21:29:05 -0300

    Changing path to files

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,44 @@
+pathToData <- file.path('..', 'UCI HAR Dataset')
+featuresFile <- file.path(pathToData, 'features.txt')
+activitiesFile <- file.path(pathToData, 'activity_labels.txt')
+trainFile <- file.path(pathToData, 'train', 'X_train.txt')
+trainActivitiesFile <- file.path(pathToData, 'train', 'y_train.txt')
+trainSubjectsFile <- file.path(pathToData, 'train', 'subject_train.txt')
+testFile <- file.path(pathToData, 'test', 'X_test.txt')
+testActivitiesFile <- file.path(pathToData, 'test', 'y_test.txt')
+testSubjectsFile <- file.path(pathToData, 'test', 'subject_test.txt')
+
 #Reading labeling data
-features   <- read.table('UCI HAR Dataset/features.txt', col.names=c('featureId', 'featureName'))
-activities <- read.table('UCI HAR Dataset/activity_labels.txt', col.names=c('activityId', 'activityLabel'))
+features   <- read.table(featuresFile, col.names=c('featureId', 'featureName'))
+activities <- read.table(activitiesFile, col.names=c('activityId', 'activityLabel'))
 featureNames   <- features[,c('featureName')]
 activityLabels <- activities[,c('activityLabel')]
 
 #Reading train and test data
-train <- read.table('UCI HAR Dataset/train/X_train.txt', col.names=featureNames)
-test  <- read.table('UCI HAR Dataset/test/X_test.txt', col.names=featureNames)
+train <- read.table(trainFile, col.names=featureNames)
+test  <- read.table(testFile,  col.names=featureNames)
 
 #Reading activity data
-trainActivities <- read.table('UCI HAR Dataset/train/y_train.txt', col.names=c('activityId'))
-testActivities  <- read.table('UCI HAR Dataset/test/y_test.txt', col.names=c('activityId'))
+trainActivities <- read.table(trainActivitiesFile, col.names=c('activityId'))
+testActivities  <- read.table(testActivitiesFile,  col.names=c('activityId'))
 
 #Reading subject data
-testSubjects  <- read.table('UCI HAR Dataset/test/subject_test.txt', col.names=c('subjectId'))
-trainSubjects <- read.table('UCI HAR Dataset/train/subject_train.txt', col.names=c('subjectId'))
+testSubjects  <- read.table(testSubjectsFile,  col.names=c('subjectId'))
+trainSubjects <- read.table(trainSubjectsFile, col.names=c('subjectId'))
 
 #Extracting only the measurements on the mean and standard deviation for each measurement
 train <- train[, c(grep('mean', names(train)), grep('std', names(train)))]
-test  <- test[, c(grep('mean', names(test)), grep('std', names(test)))]
+test  <- test[, c(grep('mean',  names(test)),  grep('std', names(test)))]
 
 #Using descriptive activity names to name the activities in the data set, binding to the train and test datasets
 train <- merge(activities, cbind(train, trainActivities), by='activityId')
-test  <- merge(activities, cbind(test, testActivities), by='activityId')
+test  <- merge(activities, cbind(test, testActivities),   by='activityId')
 
 #Creating final tidy set for activity data per subject
-test <- data.frame(type='test', testSubjects, test)
+test  <- data.frame(type='test', testSubjects, test)
 train <- data.frame(type='train', trainSubjects, train)
+names(test)  <- gsub('\\.\\.\\.', '().', names(test))
+names(train) <- gsub('\\.\\.\\.', '().', names(train))
 
 activitiesPerSubject <- rbind(train, test)
 write.table(activitiesPerSubject, file='activity_data_per_subject.txt', col.names=FALSE)
